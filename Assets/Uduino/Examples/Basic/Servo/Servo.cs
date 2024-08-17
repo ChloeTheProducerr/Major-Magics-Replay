87f5954c7064b3c1faa893e7bf63c36bd671302c
ChloeTheProducer <dragracer432@gmail.com>
2024-08-17 01:36:48 -0400

    loki the red fox

---
+++ Assets/Uduino/Examples/Basic/Servo/Servo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Uduino;
+
+public class Servo : MonoBehaviour
+{
+    public int servoPin = 1;
+    [Range(0, 180)]
+    public int servoAngle = 0;
+    private int prevServoAngle = 0;
+
+    void Start()
+    {
+        UduinoManager.Instance.pinMode(servoPin, PinMode.Servo);
+    }
+
+    void Update()
+    {
+        if (servoAngle != prevServoAngle) // Condition to not send data each frame 
+        {
+            UduinoManager.Instance.sendCommand("setServoAngle", servoPin, servoAngle);
+            prevServoAngle = servoAngle;
+        }
+    }
+}
